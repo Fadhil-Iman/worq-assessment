3a993b93429931e9a403213d2454fb04b3d7653f
Fadhil <fadhil448@gmail.com>
2026-03-05 13:02:34 +0800

    Remove 'Booking' tag from Swagger API documentation

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ async function bootstrap() {
     .setTitle('Worq Assessment')
     .setDescription('The Booking API description')
     .setVersion('1.0')
-    .addTag('Booking')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
